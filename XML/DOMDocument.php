8c8fb4a3dbb514563a53a2ae85203d626b8b6a4b
starryskywx <327466897@qq.com>
2017-04-06 18:58:02 +0800

    php

--- XML/DOMDocument.php
+++ XML/DOMDocument.php
@@ -2,4 +2,10 @@
 $doc = new DOMDocument();
 $doc->load('books.xml');
 $books = $doc->getElementsByTagName("book");
+foreach($books as $book){
+	$authors = $book->getElementsByTagName("author");
+	$author = $authors->item(0)->nodeValue;
+
+	echo "$author<br/>";
+}
 ?>
