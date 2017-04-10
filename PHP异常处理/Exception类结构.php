4be81495eec4f795888d7d03fe262380a3bcea29
starryskywx <327466897@qq.com>
2017-04-10 18:28:30 +0800

    php

---
+++ PHP异常处理/Exception类结构.php
@@ -0,0 +1,20 @@
+<?php
+class Exception{
+
+	protected string $message;
+	protected int $code;
+	protected string $file;
+	protected int $line;
+
+	public __construct($message = "", int $code = 0, $previous = NULL){}
+	final public string getMessage(void){}			//获取异常消息
+	final public Throwable getPrevious(void){}		//返回异常链中的前一个异常
+	final public int getCode(void){}				//获取异常代码
+	final public string getFile(void){}				//创建异常时的程序文件名称
+	final public int getLine(void){}				//获取创建异常所在文件的行号
+	final public array getTrace(void){}				//获取异常追中信息
+	final public string getTraceAsString(void){}	//获取字符串类型的异常追踪信息
+	public string __toString(void){}				//将异常对象转换为字符串
+	final private void __clone(void){}				//异常克隆
+}
+?>
