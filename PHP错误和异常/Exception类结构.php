57caac958f41f67eb0b3b5c43456a0334c89e3a0
starryskywx <327466897@qq.com>
2017-05-15 18:25:40 +0800

    php

--- PHP错误和异常/Exception类结构.php
+++ PHP错误和异常/Exception类结构.php
@@ -12,7 +12,7 @@ class Exception{
 	final public int getCode(void){}				//获取异常代码
 	final public string getFile(void){}				//创建异常时的程序文件名称
 	final public int getLine(void){}				//获取创建异常所在文件的行号
-	final public array getTrace(void){}				//获取异常追中信息
+	final public array getTrace(void){}				//获取异常追踪信息
 	final public string getTraceAsString(void){}	//获取字符串类型的异常追踪信息
 	public string __toString(void){}				//将异常对象转换为字符串
 	final private void __clone(void){}				//异常克隆
